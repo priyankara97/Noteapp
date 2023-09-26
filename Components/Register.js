ec1e5ca869e0e064a9f60a9e6d374ad2390ee5fe
priyankara97 <profront0tohero@gmail.com>
2023-09-26 02:03:55 -0700

    api

--- Components/Register.js
+++ Components/Register.js
@@ -1,15 +1,43 @@
 import { View, Text, TextInput, StyleSheet, Button, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
-const Register = ({navigation}) => {
+const Register = ({ navigation }) => {
+  const [userName, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const saveTravelers = () => {
+    fetch('http://192.168.8.100:3000/travelers', {
+      method: 'POST',
+      body: JSON.stringify({
+        username:userName,
+        email:email,
+        password:password,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  
+
+  };
+
+
+
+
   return (
     <View style={styles.RegForm}>
-      <TextInput placeholder='User name' style={styles.UserInput} />
-      <TextInput placeholder='Email' style={styles.UserInput} />
-      <TextInput placeholder='Password ' style={styles.UserInput} />
-      <TextInput placeholder='Confirm password' style={styles.UserInput} />
-    
-<TouchableOpacity  onPress={() => { navigation.navigate('Login') }} style={{backgroundColor:"green",color: "white", textAlign: "center", justifyContent: "center", alignItems: "center", fontSize: 18,padding:7,width:170,}}><Text style={{color:"white",fontSize: 18}}>Register</Text></TouchableOpacity>
+      <TextInput value={userName} onChangeText={(val) => {setName(val)}} placeholder='User name' style={styles.UserInput} />
+      <TextInput value={email} onChangeText={(val) => {setEmail(val)}} placeholder='Email' style={styles.UserInput} />
+      <TextInput value={password} onChangeText={(val) => {setPassword(val)}} placeholder='Password ' style={styles.UserInput} />
+      {/* <TextInput placeholder='Confirm password' style={styles.UserInput} /> */}
+      <TouchableOpacity onPress={()=>{saveTravelers()}}  style={{ backgroundColor: "green", color: "white", textAlign: "center", justifyContent: "center", alignItems: "center", fontSize: 18, padding: 7, width: 170, }}><Text style={{ color: "white", fontSize: 18 }}>Register</Text></TouchableOpacity>
+
+      {/* <TouchableOpacity onPress={() => { navigation.navigate('Login') }} style={{ backgroundColor: "green", color: "white", textAlign: "center", justifyContent: "center", alignItems: "center", fontSize: 18, padding: 7, width: 170, }}><Text style={{ color: "white", fontSize: 18 }}>Register</Text></TouchableOpacity> */}
+      <Button  onPress={()=>{saveTravelers()}} title='Save student'></Button>
 
     </View>
   )
@@ -27,8 +55,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     rowGap: 20,
-    padding:20,
-    marginTop:112
+    padding: 20,
+    marginTop: 112
   }
 
 })
